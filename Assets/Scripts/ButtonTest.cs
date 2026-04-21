85b1f66b7729d060f3398075d173ea1a362444a9
关尔 <431239560@qq.com>
2026-04-22 00:23:31 +0800

    场景

--- Assets/Scripts/ButtonTest.cs
+++ Assets/Scripts/ButtonTest.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace ns
@@ -9,9 +10,13 @@ namespace ns
     /// </summary>
     public class ButtonTest : MonoBehaviour
     {
+        public TextMeshProUGUI text;
         public void OnClick()
         {
+            float time = Time.time;
+            string s = time.ToString();
             Debug.Log("123");
+            text.text = s;
         }
     }
 }
